0e99b7c47a8d47fb69abf105fd4d1f72244b45c2
Barathsaraan <saraan.barath@gmail.com>
2023-07-12 14:17:08 +0530

    add

---
+++ main.js
@@ -0,0 +1,10 @@
+let num=prompt("Enter your Number");
+if (num>10){
+	document.write("Greater than 10");
+}
+else if(num==10){
+	document.write("Number is equal to 10");
+}
+else{
+	document.write("Number is not Greater");
+}
